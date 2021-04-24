912c46b07d4879f255e804d67ad3682182e766f3
guilherme_hilsdorf <gchilsdorf@gmail.com>
2021-04-25 00:19:27 +0100

    Porject Structure and App.js started
    Project created using Create React App, extra libraries used: react-chartjs-2. Still missing the fetch implementation, for the data is hardcoded.

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import { Line } from "react-chartjs-2";
+
+
+export default function App() {
+
+  const [data, setData] = useState({});
+  const [minValue, setMinValue] = useState(0);
+  const [region, setRegion] = useState('');
+  const [rawData, setRawData] = useState([]);
+
+
+  useEffect(() => {
+
+//TODO fetch
+
+    setRawData([
+{"id":1,"birthday":1,"spend":0,"region":"UNITED_STATES"},
+{"id":2,"birthday":2,"spend":500,"region":"UNITED_STATES"},
+{"id":3,"birthday":3,"spend":3000,"region":"EUROPE"},
+{"id":4,"birthday":5,"spend":5000,"region":"APAC"},
+    ]);
+
+  }, [])
+
+  useEffect(() => {
+
+    const filteredRawData = rawData.filter(row => row.spend >= minValue && (region === '' || row.region === region));
+
+
+    let groupByBirthday = filteredRawData.reduce((acc, it) => {
+      acc[it.birthday - 1] = acc[it.birthday - 1] + 1 || 1;
+      return acc;
+    }, []);
+
+    let groupByCumulative = filteredRawData.reduce((acc, it) => {
+      acc[it.birthday - 1] = acc[it.birthday - 1] + 5 || 5;
+
+      return acc;
+    }, []);
+
+    for (let i = 1; i < groupByCumulative.length; i++) {
+      for (let y = i - 1; y >= 0; y--) {
+        if (!!groupByCumulative[y]) {
+          groupByCumulative[i] += groupByCumulative[y];
+          break;
+        }
+      }
+    }
+
+    setData({
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Aug", "Sep", "Oct", "Nov", "Dez"],
+      datasets: [
+        {
+          label: "Monthly",
+          data: groupByBirthday,
+          fill: true,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)",
+          spanGaps: true
+        },
+        {
+          label: "Cumulative",
+          data: groupByCumulative,
+          fill: false,
+          borderColor: "#742774",
+          spanGaps: true
+        }
+      ]
+    });
+
+  }, [rawData, minValue, region]);
+
+
+
+  return (
+    <div className="App">
+      <label for="minValue">Min. Value (0-5000):</label>
+      <input type="number" min="0" max="5000" id="minValue" name="minValue" value={minValue} onChange={event => setMinValue(event.target.value)} />
+      <label for="region">Region:</label>
+      <select id="region" name="region" value={region} onChange={event => setRegion(event.target.value)} >
+        <option value="">All</option>
+        <option value="UNITED_STATES">United States</option>
+        <option value="APAC">APAC</option>
+        <option value="EUROPE">Europe</option>
+        <option value="LATIN_AMERICA">Latin America</option>
+      </select>
+      <Line data={data} />
+    </div>
+  );
+}
